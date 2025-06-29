d060a0ad9ee1ec9db88c7e677b80645bfe1d953e
SebasRamirez97 <ramirez.sebas97@gmail.com>
2025-06-29 12:40:20 -0300

    Estructura del juego

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ pygame.init()
 
 ventana = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Ventana controlada")
+cajita = pygame.surface((100,100))
+
 
 corriendo = True
 
@@ -13,8 +15,11 @@ while corriendo:
             corriendo = False
 
     # Acá se refresca la pantalla en cada ciclo, no por cada evento
-    ventana.fill((0, 0, 0))  # Fondo negro
+        ventana.fill((0, 0, 0))  # Fondo negro
+    cajita.fill((255,0,0))
     pygame.display.update()
+    
+    
 
 pygame.quit()
 

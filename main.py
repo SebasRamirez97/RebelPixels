40f564951a98d27b185ed795abee0e000511e22f
SebasRamirez97 <ramirez.sebas97@gmail.com>
2025-06-30 19:28:25 -0300

    Empaquetado de naves

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ pygame.init()
 
 ventana = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Ventana controlada")
-
+(caza, bombardero, fragata, carrier, nodriza) = e_image()
 
 
 corriendo = True
@@ -21,18 +21,21 @@ while corriendo:
     # Acá se refresca la pantalla en cada ciclo, no por cada evento
     ventana.fill((50, 50, 50))  # Fondo gris
     ventana.blit(p_image(),(360,500))#pegado de jugador en el fondo
+    
+    #diseño de formacion de naves enemigas
     x0 = 300
     x1 = 280
     x2 = 230
 
     for i in range(0,3):    
-        ventana.blit(e_image()[0],(x0,200))
-        ventana.blit(e_image()[1],(x1,150))
+        ventana.blit(caza,(x0,200))
+        ventana.blit(bombardero,(x1,150))
         x0 += 50
         x1 += 70
     for i in range(0,2):
-        ventana.blit(e_image()[2],(x2,50))
+        ventana.blit(fragata,(x2,50))
         x2 += 190
+    
     pygame.display.flip()
     pygame.time.Clock().tick(60)
     

1886a1cd22c630f2897e246a6dc8416beabebf8d
Yamila Pereira <pereira.yamila@hotmail.com>
2025-07-04 00:25:30 -0300

    def para puntaje y enemigos destruidos

--- scripts/funciones_comunes.py
+++ scripts/funciones_comunes.py
@@ -9,3 +9,15 @@ def crear_mascara(superficie):
     mascara_superficie = pygame.mask.from_surface(superficie)
 
     return mascara_superficie
+
+
+def mostrar_puntuacion(superficie, fuente, puntuacion, color=(255, 255, 255), posicion=(10, 20)):
+    texto = fuente.render(f"Puntaje: {puntuacion}", True, color)
+    superficie.blit(texto, posicion)
+    
+
+def enemigos_destruidos(superficie, fuente, cantidad, color=(255, 255, 255), posicion=(10, 50)):
+    texto = fuente.render(f"Enemigos destruidos: {cantidad}", True, color)
+    superficie.blit(texto, posicion)
+    
+        
